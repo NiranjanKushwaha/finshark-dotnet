b9d8cb09c1e6346a23b36358f97dd025e265eea0
Niranjan Kushwaha <0niranjankumar1@gmail.com>
2025-01-26 11:32:42 +0530

    created post request to create a new stock

--- api/Mappers/StockMappers.cs
+++ api/Mappers/StockMappers.cs
@@ -9,15 +9,30 @@ namespace api.Mappers
 {
     public static class StockMappers
     {
-        public static StockDto ToStockDto(this Stock stockModel){
-            return new StockDto{
-                 Id = stockModel.Id,
-                 Symbol = stockModel.Symbol,
-                 CompanyName = stockModel.CompanyName,
-                 Purchase = stockModel.Purchase,
-                 Divident = stockModel.Divident,
-                 Industry = stockModel.Industry,
-                 MarketCap = stockModel.MarketCap,
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                Divident = stockModel.Divident,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap,
+            };
+        }
+
+        public static Stock ToStockEntity(this CreateStockRequestDto stockRequestDto)
+        {
+            return new Stock
+            {
+                Symbol = stockRequestDto.Symbol,
+                CompanyName = stockRequestDto.CompanyName,
+                Purchase = stockRequestDto.Purchase,
+                Divident = stockRequestDto.Divident,
+                Industry = stockRequestDto.Industry,
+                MarketCap = stockRequestDto.MarketCap,
             };
         }
     }
